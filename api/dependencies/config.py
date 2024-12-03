5b09fb4e53c3544464ec00ff6d1355677572393e
rgutknec4949 <rgutknec@charlotte.edu>
2024-12-03 11:17:31 -0500

    fixed attribute error with promo_id

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -1,8 +1,8 @@
 class conf:
     db_host = "localhost"
-    db_name = "skeleton"
+    db_name = "discord_alliance"
     db_port = 3306
     db_user = "root"
-    db_password = "topathupha"
+    db_password = "Get2SQL!"
     app_host = "localhost"
     app_port = 8000
